a43d591ec309be01d9abc5d3c1c6db798d960e03
Nadeem Bitar <nadeem@gmail.com>
2020-12-10 07:11:37 -0800

    🎉 Run postgresql with postgis inside nix-shell

---
+++ shell.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+mkShell {
+  name = "local-postgresql";
+  buildInputs = [
+        (pkgs.postgresql.withPackages (p: [ p.postgis ]))
+    ];
+
+   shellHook = ''
+        export PGHOST="$PWD/db"
+        export PGDATA="$PGHOST/db"
+        export PGLOG=$PGHOST/postgres.log
+
+        mkdir -p $PGHOST
+
+        if [ ! -d $PGDATA ]; then
+          initdb --auth=trust --no-locale --encoding=UTF8
+        fi
+
+        if ! pg_ctl status
+        then
+          pg_ctl start -l $PGLOG -o "--unix_socket_directories='$PGHOST'"
+        fi
+
+       function end {
+          echo "Shutting down local-postgresql..."
+          pg_ctl stop
+        }
+        trap end EXIT
+    '';
+}
