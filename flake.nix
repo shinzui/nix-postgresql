201393658c7ca3092abf6f13632a6e72a732c418
Nadeem Bitar <nadeem@gmail.com>
2021-07-15 07:29:33 -0700

    ❄ Convert to flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "PostgreSQL database for development";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
--- shell.nix
+++ shell.nix
@@ -1,30 +1,31 @@
-with import <nixpkgs> {};
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
 mkShell {
   name = "local-postgresql";
   buildInputs = [
     (postgresql_12.withPackages (p: [ p.postgis ]))
-    ];
+  ];
 
-   shellHook = ''
-        export PGHOST="$PWD/db"
-        export PGDATA="$PGHOST/db"
-        export PGLOG=$PGHOST/postgres.log
+  shellHook = ''
+     export PGHOST="$PWD/db"
+     export PGDATA="$PGHOST/db"
+     export PGLOG=$PGHOST/postgres.log
 
-        mkdir -p $PGHOST
+     mkdir -p $PGHOST
 
-        if [ ! -d $PGDATA ]; then
-          initdb --auth=trust --no-locale --encoding=UTF8
-        fi
+     if [ ! -d $PGDATA ]; then
+       initdb --auth=trust --no-locale --encoding=UTF8
+     fi
 
-        if ! pg_ctl status
-        then
-          pg_ctl start -l $PGLOG -o "--unix_socket_directories='$PGHOST'"
-        fi
+     if ! pg_ctl status
+     then
+       pg_ctl start -l $PGLOG -o "--unix_socket_directories='$PGHOST'"
+     fi
 
-       function end {
-          echo "Shutting down local-postgresql..."
-          pg_ctl stop
-        }
-        trap end EXIT
-    '';
+    function end {
+       echo "Shutting down local-postgresql..."
+       pg_ctl stop
+     }
+     trap end EXIT
+  '';
 }
